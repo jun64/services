d3464ea7561dceaf2ba6047f7aa089919e12843b
jun64 <mahaha94@gmail.com>
2021-06-04 14:43:16 +0900

    section 9 before assignment

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,26 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { AccountsService } from '../accounts.service';
+
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor(private loggingService: LoggingService,
+    private accountService: AccountsService) {
+      this.accountService.statusUpdated.subscribe(
+        (status: string) => alert('New Status : ' + status)
+      )
+    }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.accountService.addAccount(accountName, accountStatus)
+    // this.loggingService.logStatusChage(accountStatus);
+
   }
 }
